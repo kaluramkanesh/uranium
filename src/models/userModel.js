efe134f2d0f617f72b9f0aa9c9bd79c19a2f1e3d
HemrajDeshmukh123 <hemrajd570@gmail.com>
2022-04-22 08:20:45 +0530

    21April  Assignment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,3 +1,28 @@
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+
+//         required: true
+//     },
+//     emailId: String,
+//     password: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "other"]
+//     },
+//     isDeleted: {
+//         type: Boolean,
+//         default: false
+//     },
+//     age: Number,
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema)
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
@@ -13,6 +38,10 @@ const userSchema = new mongoose.Schema( {
     gender: {
         type: String,
         enum: ["male", "female", "other"]
+    },
+    isdeleted:{
+        default: false,
+
     },
     age: Number,
 }, { timestamps: true });
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,23 @@
+// const express = require('express');
+// const router = express.Router();
+// const userController= require("../controllers/userController")
+
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
+
+// router.post("/users", userController.createUser  )
+
+// router.post("/login", userController.loginUser)
+
+// //The userId is sent by front end
+// router.get("/users/:userId", userController.getUserData)
+
+// router.put("/users/:userId", userController.updateUser)
+
+// module.exports = router;
+
+
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
@@ -14,5 +34,7 @@ router.post("/login", userController.loginUser)
 router.get("/users/:userId", userController.getUserData)
 
 router.put("/users/:userId", userController.updateUser)
+
+router.put("/deleted/:userId", userController.isdeleted)
 
 module.exports = router;
